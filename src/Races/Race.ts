ff41027205dc1a26c70b051aa50abe1176ef72f3
no_name <no@email.com>
2024-09-29 17:41:21 -0300

    Requisito 1 satisfeito

---
+++ src/Races/Race.ts
@@ -0,0 +1,9 @@
+export default abstract class Race {
+  static _createdInstances = 0;
+  constructor(private _name: string, private _dexterity: number) {}
+
+  get name(): string { return this._name; }
+  get dexterity(): number { return this._dexterity; }
+  static createdRacesInstances(): number { throw new Error('Not implemented'); }
+  abstract get maxLifePoints(): number;
+}
